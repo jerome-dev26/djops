779f0b6ff2a336d76cf5a588dde9eb3aa617ae0c
jerome-dev26 <jdevmastet@gmail.com>
2026-05-02 14:40:45 +0300

    Update main.tf
    Enable DNS for VPC endpoints

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,7 +1,18 @@
-resource "aws_vpc" "this" {
+/*resource "aws_vpc" "this" {
   cidr_block = "10.0.0.0/16"
 
   tags = { Name = "secure-vpc" }
+}
+*/
+resource "aws_vpc" "this" {
+  cidr_block = "10.0.0.0/16"
+
+  enable_dns_support   = true   # ✅ REQUIRED
+  enable_dns_hostnames = true   # ✅ REQUIRED
+
+  tags = {
+    Name = "secure-vpc"
+  }
 }
 
 resource "aws_subnet" "public" {
