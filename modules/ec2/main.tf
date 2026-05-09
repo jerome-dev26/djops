1cf8a0c58d760f2219b69b46622a597fbf3844df
Jeromewachira <moneyjerome1.com>
2026-05-09 07:42:30 +0300

    Resolve Add ALB for docker app

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -34,7 +34,7 @@ resource "aws_security_group" "ec2_sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    security_groups = [aws_security_group.alb_sg]
+    security_groups = [aws_security_group.alb_sg.id]
   }
 
   egress {
